e176ed6fe4925caa1fb747458629ad49a8237a12
zhengjiong <zhengjiong2006@gmail.com>
2017-06-05 20:20:42 +0800

    init project

---
+++ app/src/main/java/com/zj/example/tinker/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zj.example.tinker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView tvTitle = findViewById(R.id.title);
+    }
+}
